88b7bad8c51139549b403120f424432f072f43c1
emrysf <1@fxcdev.com>
2019-01-27 22:12:45 +0800

    [feature] add Beatiful girl Project

---
+++ BeatifulGirl/BeatifulGirl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeatifulGirl
+//
+//  Created by Emrys on 2019/1/27.
+//  Copyright © 2019 Emrys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
